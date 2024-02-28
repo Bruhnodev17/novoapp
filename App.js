925f2e5a5c7504ce6caddc0b0634f4895e5b33a6
Manoel Bruno <manoelbrunop17@gmail.com>
2024-02-27 22:51:50 -0300

    Commit do primeiro app

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet} from 'react-native'
 
 export default function App() {
-  return (
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello World DEV!</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  container:{
+    flex:1,
+    backgroundColor: "#F3F3FF",
     justifyContent: 'center',
-  },
-});
+    alignItems: 'center'
+  }
+})
