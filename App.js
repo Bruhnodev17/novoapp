46cc7ac975022a82478ff5bacff327cef703c5cf
Manoel Bruno <manoelbrunop17@gmail.com>
2024-02-28 12:24:22 -0300

    Tela inicial, estática

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import { View, Text, StyleSheet, Image} from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native'
+import Slider from '@react-native-community/slider'
 
 export default function App() {
   return(
@@ -8,9 +9,23 @@ export default function App() {
 
       <Text style={styles.title}>20 caracteres</Text>
 
-      <View>
-        
+      <View style={styles.area}>
+        <Slider 
+        style={{height: 50}}
+        minimumValue={10}
+        maximumValue={20}
+        maximumTrackTintColor="#ff0000"
+        minimumTrackTintColor="#000"
+        thumbTintColor="#392de9"
+        />
       </View>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>
+          Gerar Senha
+        </Text>
+      </TouchableOpacity>
+      
     </View>
   )
 }
@@ -26,6 +41,27 @@ const styles = StyleSheet.create({
     marginBottom:60,
   },
   title:{
-    fontSize: 40,
-  }
+    fontSize: 30,
+  },
+  area:{
+    marginTop: 14,
+    marginBottom: 14,
+    width: "80%",
+    backgroundColor: "#FFF",
+    borderRadius: 8,
+    padding: 8,
+  },
+  button:{
+    backgroundColor: "#392de9",
+    width: "80%",
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+    marginBottom: 18,
+  },
+  buttonText:{
+    color: "#FFF",
+    fontSize: 20,
+  },
 })
