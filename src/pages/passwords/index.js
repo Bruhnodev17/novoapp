5cca0b93fd73a145ddafcb484812e9bda112af8f
Manoel Bruno <manoelbrunop17@gmail.com>
2024-03-01 12:17:03 -0300

    função para deletar itens da lista

--- src/pages/passwords/index.js
+++ src/pages/passwords/index.js
@@ -2,20 +2,29 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useState, useEffect } from "react";
 import { useIsFocused } from "@react-navigation/native";
-import useStorage from "../../hooks/useStorage"
+import useStorage from "../../hooks/useStorage";
+
+import {PasswordItem} from "./components/passwordItem"
 
 export function Passwords() {
   const [listPasswords, setListPasswords] = useState([]);
-  const focused = useIsFocused()
-  const {getItem} = useStorage()
+
+  const focused = useIsFocused();
+  const { getItem, removeItem } = useStorage();
 
   useEffect(() => {
-    async function loadPasswords(){
-        const passwords = await getItem("@pass")
-        setListPasswords(passwords)
+    async function loadPasswords() {
+      const passwords = await getItem("@pass");
+      setListPasswords(passwords);
     }
-    loadPasswords()
+    loadPasswords();
   }, [focused]);
+
+
+  async function handleDeletePassword(item){
+    const passwords = await removeItem("@pass", item)
+    setListPasswords(passwords)
+  }
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -24,10 +33,11 @@ export function Passwords() {
       </View>
 
       <View style={styles.content}>
-        <FlatList 
-        data={listPasswords}
-        keyExtractor={(item)=> String(item)}
-        renderItem={({item})=> <Text>{item}</Text>}
+        <FlatList
+          style={{ flex: 1, paddingTop: 14 }}
+          data={listPasswords}
+          keyExtractor={(item) => String(item)}
+          renderItem={({ item }) => <PasswordItem data={item} removePassword={()=> handleDeletePassword(item)}/>}
         />
       </View>
     </SafeAreaView>
@@ -47,5 +57,10 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#FFF",
     textAlign: "center",
+  },
+  content: {
+    flex: 1,
+    paddingLeft: 14,
+    paddingRight: 14,
   },
 });
